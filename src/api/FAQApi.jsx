5ef89eca8240a5ae4eb616f8dada0b4fa4bb9300
I511494 <m.huseynov@student.fontys.nl>
2024-05-28 09:44:58 +0200

    quick fix

--- src/api/FAQApi.jsx
+++ src/api/FAQApi.jsx
@@ -1,4 +1,34 @@
+import axios from "axios";
+import TokenManager from "./TokenManager";
+
+const baseURL = "http://localhost:8080"; 
+
 const FAQApi = {
+    getFAQs: () => axios.get(`${baseURL}/faqs`, {
+        headers: {
+            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
+        }
+    }).then((result) => result.data),
+    
+
+    createFAQ: (newFAQ) => axios.post(`${baseURL}/faqs`, newFAQ, {
+        headers: {
+            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
+        }
+    }),
+
+    deleteFAQ: (faqId) => axios.delete(`${baseURL}/faqs/${faqId}`, {
+        headers: {
+            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
+        }
+    }),
+
+    updateFAQ: (id, updatedFAQ) => axios.put(`${baseURL}/faqs/${id}`, updatedFAQ, {
+        headers: {
+            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
+        }
+    }),
+
     getFAQStatistics: async () => {
         const response = await fetch('http://localhost:8080/faqs/statistics');
         return await response.json();
