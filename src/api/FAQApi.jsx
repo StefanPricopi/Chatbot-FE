cff21cc493abc9beba1eb9e2d35ac85789e57522
I511494 <m.huseynov@student.fontys.nl>
2024-05-28 09:38:52 +0200

    Statistics styling

--- src/api/FAQApi.jsx
+++ src/api/FAQApi.jsx
@@ -1,46 +1,28 @@
-import axios from "axios";
-import TokenManager from "./TokenManager";
-
-const baseURL = "http://localhost:8080"; 
-
 const FAQApi = {
-    getFAQs: () => axios.get(`${baseURL}/faqs`, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }).then((result) => result.data),
-
-    createFAQ: (newFAQ) => axios.post(`${baseURL}/faqs`, newFAQ, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }),
-
-    deleteFAQ: (faqId) => axios.delete(`${baseURL}/faqs/${faqId}`, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }),
-
-    updateFAQ: (id, updatedFAQ) => axios.put(`${baseURL}/faqs/${id}`, updatedFAQ, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }),
-
-    getFAQStatistics: () => axios.get(`${baseURL}/faqs/statistics`, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }).then((result) => result.data)
-        .catch((error) => console.error("Error fetching FAQ statistics:", error)),
-
-    getOutOfOfficeChats: () => axios.get(`${baseURL}/faqs/outOfOfficeChats`, {
-        headers: {
-            'Authorization': `Bearer ${TokenManager.getAccessToken()}`
-        }
-    }).then((result) => result.data)
-        .catch((error) => console.error("Error fetching out-of-office chat count:", error)),
+    getFAQStatistics: async () => {
+        const response = await fetch('http://localhost:8080/faqs/statistics');
+        return await response.json();
+    },
+    getOutOfOfficeChats: async () => {
+        const response = await fetch('http://localhost:8080/faqs/outOfOfficeChats');
+        return await response.json();
+    },
+    getFailedQuestions: async () => {
+        const response = await fetch('http://localhost:8080/faqs/failedQuestions');
+        return await response.json();
+    },
+    getRoutedChats: async () => {
+        const response = await fetch('http://localhost:8080/faqs/routedChats');
+        return await response.json();
+    },
+    getChatsWithoutHuman: async () => {
+        const response = await fetch('http://localhost:8080/faqs/chatsWithoutHuman');
+        return await response.json();
+    },
+    getChatsDuringOutOfOffice: async () => {
+        const response = await fetch('http://localhost:8080/faqs/chatsDuringOutOfOffice');
+        return await response.json();
+    }
 };
 
 export default FAQApi;
